03d5f265c43a386fec14eebd8b5533aace98b4da
mossymagnesium <n8n2004@gmail.com>
2023-07-22 21:21:01 -0500

    initial commit

--- src/main/java/net/mcreator/contortion/init/ContortionModBlocks.java
+++ src/main/java/net/mcreator/contortion/init/ContortionModBlocks.java
@@ -10,10 +10,12 @@ import net.minecraftforge.registries.DeferredRegister;
 
 import net.minecraft.world.level.block.Block;
 
+import net.mcreator.contortion.block.MalaisedDesolateDirtBlock;
 import net.mcreator.contortion.block.ContortstoneBlock;
 import net.mcreator.contortion.ContortionMod;
 
 public class ContortionModBlocks {
 	public static final DeferredRegister<Block> REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, ContortionMod.MODID);
 	public static final RegistryObject<Block> CONTORTSTONE = REGISTRY.register("contortstone", () -> new ContortstoneBlock());
+	public static final RegistryObject<Block> MALAISED_DESOLATE_DIRT = REGISTRY.register("malaised_desolate_dirt", () -> new MalaisedDesolateDirtBlock());
 }
--- src/main/java/net/mcreator/contortion/init/ContortionModItems.java
+++ src/main/java/net/mcreator/contortion/init/ContortionModItems.java
@@ -18,6 +18,7 @@ import net.mcreator.contortion.ContortionMod;
 public class ContortionModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, ContortionMod.MODID);
 	public static final RegistryObject<Item> CONTORTSTONE = block(ContortionModBlocks.CONTORTSTONE, CreativeModeTab.TAB_DECORATIONS);
+	public static final RegistryObject<Item> MALAISED_DESOLATE_DIRT = block(ContortionModBlocks.MALAISED_DESOLATE_DIRT, ContortionModTabs.TAB_CONTORTION_BLOCKS);
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
